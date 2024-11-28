945b7600a90e34e24580cc30c4c02ceec931e5dc
Luciano Enrique Mejia Pretell <lmejiapretell@gmail.com>
2024-11-28 12:09:30 -0500

    Agregar archivos de proyecto.

---
+++ Calculadora/Calculadora.cpp
@@ -0,0 +1,75 @@
+
+#include <iostream>
+#include<windows.h>
+
+using namespace std;
+
+
+void sumar() {
+
+}
+
+void resta() {
+
+}
+
+void multi() {
+
+}
+
+void divi() {
+
+}
+
+void poten() {
+
+}
+
+void raiz() {
+
+}
+
+void opCombi() {
+
+}
+
+
+
+
+
+int main()
+{
+	int a;
+
+	cout << "***************MENU*************" << endl;
+	cout << "1.Sumar" << endl;
+	cout << "2.Resta" << endl;
+	cout << "3.Multiplicacion" << endl;
+	cout << "4.Division" << endl;
+	cout << "5.Potencia" << endl;
+	cout << "6.Raiz" << endl;
+	cout << "7.Operacion Combinada" << endl;
+
+	cout << "Ingrese su seleccion: "; cin >> a;
+
+	switch (a)
+	{
+	case 1:
+
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+
+	}
+}
+
